4c771a577c04d4d46272395eac1778d536fbc9d0
Matt Latimer <matt.latimer@gmail.com>
2017-09-11 14:35:07 -0700

    improved user experience

--- http-request.js
+++ http-request.js
@@ -8,6 +8,13 @@ request.get('https://sytantris.github.io/http-examples/future.jpg')
   .on('response', (response) => {
     console.log('Response Status Code:', response.statusCode,
       '\nResponse Message:', response.statusMessage,
-      '\nContent Type:', response.headers['content-type']);
+      '\nContent Type:', response.headers['content-type'],
+      '\nDownloading...');
   })
-  .pipe(fs.createWriteStream('./future.jpg'));
+  .on('end', () => {
+    console.log('Download Complete.');
+  })
+  .pipe(fs.createWriteStream('./future.jpg')
+  .on('finish', () => {
+    console.log('File written to disk.');
+  }));
